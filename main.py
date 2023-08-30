ebd6bc0a088353dee0e4c0ed455a8ba4362e56ac
Felix-Okeyo <felixokeyo20@gmail.com>
2023-08-30 13:20:48 +0300

    completw

--- main.py
+++ main.py
@@ -40,3 +40,48 @@ test_expression2 = "([)]"
 print(is_balanced(test_expression1))  # Output: True
 print(is_balanced(test_expression2))  # Output: False
 
+
+# Sequences (Lists/Tuples): 
+
+# Question 2: Write a function remove_duplicates(sequence) that takes a 
+# sequence (list or tuple) and returns a new sequence with duplicates 
+# removed while maintaining the original order of elements. 
+
+# sample input = 
+
+# input_sequence = [2, 3, 2, 4, 5, 3, 6, 7, 5]
+# result = remove_duplicates(input_sequence)
+# print(result)  # Output: [2, 3, 4, 5, 6, 7]
+
+def remove_duplicates(sequence):
+    return list(dict.fromkeys(sequence))
+
+input_sequence = [2, 3, 2, 4, 5, 3, 6, 7, 5]
+result = remove_duplicates(input_sequence)
+print(result)  # Output: [2, 3, 4, 5, 6, 7]
+
+
+# Dictionaries: 
+
+# Question 3: Implement a function word_frequency(sentence) that takes 
+# a sentence and returns a dictionary containing the frequency of each 
+# word in the sentence. 
+
+# Ignore punctuation and consider words in a case-insensitive manner. 
+
+# sample input = 
+
+# sentence = "This is a test sentence. This sentence is a test."
+# result = word_frequency(sentence)
+# print(result)
+
+import re
+
+def word_frequency(sentence):
+    words = re.sub("[^\w]", " ",  sentence.lower()).split()
+    frequencies = {word: words.count(word) for word in words}
+    return frequencies
+
+sentence = "This is a test sentence. This sentence is a test."
+result = word_frequency(sentence)
+print(result)
